29f56f3891f34233a68a5cd81f6e405ab7f56400
chavhan1615 <146634527+chavhan1615@users.noreply.github.com>
2024-03-06 20:41:40 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+
+mongoose.set('strictQuery', false);
+
 const db = mongoose.connect("mongodb+srv://shubham1615:shubham123@mobileapp.tkbshm4.mongodb.net/?retryWrites=true&w=majority&appName=MobileApp",{
     // useUnifiedTopology: true,
     // useCreateIndex: true,

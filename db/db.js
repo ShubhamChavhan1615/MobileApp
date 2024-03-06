987d4df5b95a93fd4685976c14b4e42412f5c355
chavhan1615 <146634527+chavhan1615@users.noreply.github.com>
2024-03-06 17:09:12 +0530

    database
    add

---
+++ db/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const db = mongoose.connect("mongodb+srv://shubham1615:shubham123@mobileapp.tkbshm4.mongodb.net/?retryWrites=true&w=majority&appName=MobileApp",{
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+})
+.then(()=>{
+    console.log("connected")
+})
+.catch((err)=>{
+    console.log("error is ", err)
+})
+
+
+export default db;
